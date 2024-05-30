379fc38523b94bcd9e3dd06477456ee07f0242da
Maysa Leocadio <lmaysa.gomes@gmail.com>
2024-05-29 21:44:59 -0300

    add routes logic

---
+++ router/routes.go
@@ -0,0 +1,21 @@
+package router
+
+import "github.com/gin-gonic/gin"
+
+func initializeRoutes(router *gin.Engine) {
+	// Initialize Handler
+	handler.InitializeHandler()
+	basePath := "/api/v1"
+	docs.SwaggerInfo.BasePath = basePath
+	v1 := router.Group(basePath)
+	{
+		v1.GET("/opening", handler.ShowOpeningHandler)
+		v1.POST("/opening", handler.CreateOpeningHandler)
+		v1.DELETE("/opening", handler.DeleteOpeningHandler)
+		v1.PUT("/opening", handler.UpdateOpeningHandler)
+		v1.GET("/openings", handler.ListOpeningsHandler)
+
+	}
+	// Initialize Swagger
+	router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerfiles.Handler))
+}
